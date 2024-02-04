d1ac7a6999df474e30c4cae053a766047bbf14a5
Ritesh Koushik <riteshkoushik39@gmail.com>
2024-02-04 20:59:40 +0530

    Generate and load secret key

---
+++ generate-keypair/view-keypair.ts
@@ -0,0 +1,8 @@
+import "dotenv/config";
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+
+// console.log(process.env);
+const keypair = getKeypairFromEnvironment("SECRET_KEY");
+
+console.log("Finished! We've loaded our secret key securely, using an env file!");
+
